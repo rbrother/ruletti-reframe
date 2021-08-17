5fb1e1d4b354ac4ebc36ef3e39611399b64966fc
Robert Brotherus <robert@iki.fi>
2021-08-17 18:20:14 +0300

    Adding simple minimal leiningen project.clj so that InitelliJ Cursive can handle intellisense for the codebase ok
    We are not really using Leiningen for building this 100% shadow-cljs project, so this
    duplicates some definitions in shadow-cljs.edn, but necessary for time being

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject verto-gui "1.0"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773"
+                  :exclusions [com.google.javascript/closure-compiler-unshaded
+                               org.clojure/google-closure-library
+                               org.clojure/google-closure-library-third-party]]
+                 [re-frame "1.2.0"]])
